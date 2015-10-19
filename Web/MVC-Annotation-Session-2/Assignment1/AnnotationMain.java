12f5cafda9508f9d6b50c9ad201ca397402d7cd8
Banwari kevat <banwari.kevat@metacube.com>
2015-10-19 17:24:58 +0530

    Update AnnotationMain.java

--- Web/MVC-Annotation-Session-2/Assignment1/AnnotationMain.java
+++ Web/MVC-Annotation-Session-2/Assignment1/AnnotationMain.java
@@ -3,7 +3,8 @@
  * @author Banwari Kevat
  */
 public class AnnotationMain {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception 
+	{
 		TestAnnotationParser parser = new TestAnnotationParser();
 		parser.parse(Employee.class);
 	}
